eba46ae1523d385a1d6fdb4450228e08b8577905
sakif <sakifsahriar@gmail.com>
2022-08-08 12:00:03 +0600

    first commit

---
+++ Assembly Language/Simple_Assembly_with_starup/main.s
@@ -0,0 +1,14 @@
+;This is a simple project
+
+	AREA SimpleProject , CODE,READONLY
+	ENTRY
+	EXPORT __main
+	
+__main
+		MOV		R5,#0x1234
+		MOV		R3,#0x1234
+		
+		ADD		R6,R5,R3
+		
+STOP	B		STOP
+		END
